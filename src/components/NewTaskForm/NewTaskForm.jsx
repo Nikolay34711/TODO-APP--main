71b35c6e9f048e4a9cdf184d63f8ab46796ade2f
Nikolay <149093882+NMKRTYCHIAN@users.noreply.github.com>
2024-01-19 10:08:34 +0300

    refactoring

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './NewTaskForm.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './NewTaskForm.css'
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class NewTaskForm extends Component {
-
   state = {
     field: '',
-  };
+  }
 
   onChange = (e) => {
     this.setState({
       field: e.target.value,
-    });
-  };
+    })
+  }
 
   onSubmit = (e) => {
-    e.preventDefault();
-    const { field } = this.state;
-    const { addTask } = this.props;
-    addTask(field);
-    this.setState(() => ({ field: '' }));
-  };
+    e.preventDefault()
+    const { field } = this.state
+    const { addTask } = this.props
+    if (field.trim().length === 0) {
+      console.log('пусто')
+    } else {
+      addTask(field)
+    }
+    this.setState(() => ({ field: '' }))
+  }
 
   render() {
-    
-    const { field } = this.state;
+    const { field } = this.state
 
     return (
       <form className="header" onSubmit={this.onSubmit}>
-
         <h1>Todos</h1>
 
         <input className="new-todo" value={field} onChange={this.onChange} placeholder="What needs to be done?" />
-        
       </form>
-    );
+    )
   }
 }
 
 NewTaskForm.propTypes = {
   addTask: PropTypes.func.isRequired,
-};
+}
